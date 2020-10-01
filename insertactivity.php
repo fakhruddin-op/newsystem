bc707f63905a8412188465ff040e87880236f4e5
Luqman <luqmanhaqeemsamsul@gmail.com>
2020-10-01 20:41:24 +0800

    Auto stash before merge of "master" and "origin/master"

--- insertactivity.php
+++ insertactivity.php
@@ -41,15 +41,8 @@ if(isset($_POST['isbn']) &&
 	$bookcodesubject=$_POST['bookcodesubject'];
 
 	//sql insert
-<<<<<<< HEAD
-	$sql="INSERT INTO orderbook
-		( isbn, bookname, bookcodesubject, bookcover,status)
-		VALUES('$isbn','$bookname',
-		'$bookcodesubject','$newfilename','not approved')";
-=======
 	$sql="INSERT INTO orderbook ( isbn, bookname, bookcodesubject)
 		VALUES('$isbn','$bookname','$bookcodesubject')";
->>>>>>> master
 		//data dari borang html
 		//echo $sql;
 echo "Mysql error:".mysqli_error($conn);
