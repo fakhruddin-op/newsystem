e04ad58e3f520504eafdda49cacad8531a70c50b
fakhruddin-op <70927031+fakhruddin-op@users.noreply.github.com>
2020-10-03 16:13:39 +0800

    fyp

--- fyp.sql
+++ fyp.sql
@@ -13,10 +13,12 @@
 
 
 -- Dumping database structure for newsystem
+DROP DATABASE IF EXISTS `newsystem`;
 CREATE DATABASE IF NOT EXISTS `newsystem` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `newsystem`;
 
 -- Dumping structure for table newsystem.bookrequest
+DROP TABLE IF EXISTS `bookrequest`;
 CREATE TABLE IF NOT EXISTS `bookrequest` (
   `ownerid` int(11) DEFAULT NULL,
   `idbbok` int(11) DEFAULT NULL,
@@ -27,7 +29,21 @@ CREATE TABLE IF NOT EXISTS `bookrequest` (
 /*!40000 ALTER TABLE `bookrequest` DISABLE KEYS */;
 /*!40000 ALTER TABLE `bookrequest` ENABLE KEYS */;
 
+-- Dumping structure for table newsystem.forgotpassword
+DROP TABLE IF EXISTS `forgotpassword`;
+CREATE TABLE IF NOT EXISTS `forgotpassword` (
+  `reset_id` int(50) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `reser_token` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`reset_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table newsystem.forgotpassword: ~0 rows (approximately)
+/*!40000 ALTER TABLE `forgotpassword` DISABLE KEYS */;
+/*!40000 ALTER TABLE `forgotpassword` ENABLE KEYS */;
+
 -- Dumping structure for table newsystem.orderbook
+DROP TABLE IF EXISTS `orderbook`;
 CREATE TABLE IF NOT EXISTS `orderbook` (
   `idbook` int(11) NOT NULL AUTO_INCREMENT,
   `status` varchar(50) NOT NULL DEFAULT '0',
@@ -37,36 +53,20 @@ CREATE TABLE IF NOT EXISTS `orderbook` (
   `bookcodesubject` varchar(50) DEFAULT NULL,
   `bookcover` varchar(225) DEFAULT NULL,
   PRIMARY KEY (`idbook`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
--- Dumping data for table newsystem.orderbook: ~22 rows (approximately)
+-- Dumping data for table newsystem.orderbook: ~5 rows (approximately)
 /*!40000 ALTER TABLE `orderbook` DISABLE KEYS */;
 INSERT INTO `orderbook` (`idbook`, `status`, `ownerid`, `isbn`, `bookname`, `bookcodesubject`, `bookcover`) VALUES
-	(1, '0', 2, '232133', 'sejarah', 'bws124', NULL),
-	(2, '0', 2, '232133', 'sejarah', 'bws124', NULL),
-	(3, '0', 0, '123', 'bm', 'bws12445', NULL),
-	(4, '0', 0, '123', 'bm', 'bws12445', NULL),
-	(5, '0', 0, '232133', 'bm', '123ed', NULL),
-	(6, '0', 0, '232133', 'bm', 'bws124', NULL),
-	(7, '0', 0, '12321', 'sejarah', 'bws12445', NULL),
-	(8, '0', 0, '12321', 'sejarah', 'bws12445', NULL),
-	(9, '0', 0, '12321', 'sejarah', 'bws12445', NULL),
-	(10, '0', 0, '123', 'sejarah', 'bws12445', NULL),
-	(11, '0', 0, '123', 'sejarah', 'bws12445', NULL),
-	(12, '0', 0, '232133', 'sejarah', 'bws124', NULL),
-	(13, '0', 0, '232133', 'sejarah', 'bws124', NULL),
-	(14, '0', 0, '232133', 'sejarah', 'bws124', NULL),
-	(15, '0', 0, '12321', 'bm', 'bws12445', NULL),
-	(16, '0', 0, '12321', 'bm', 'bws124', NULL),
-	(17, '0', 0, '12321', 'bm', 'bws124', NULL),
-	(18, '0', 0, '125', 'latihan', 'qwe23', NULL),
-	(19, '0', 0, '125', 'latihan', 'qwe23', NULL),
-	(20, '0', 0, '12321', 'sejarah', 'bws124', NULL),
-	(21, '0', 0, '232133', 'sejarah', 'bws124', NULL),
-	(22, '0', 0, '232133', 'latihan', 'bws124', '03-09-2020-10774e9a568a1541c49a847ec043cc3e1579855169-lg.jpg');
+	(1, '0', 3, '123', 'bm', 'bws124', NULL),
+	(2, '0', 3, '123', 'bm', 'bws124', NULL),
+	(3, '0', 3, '123', 'bm', 'bws124', NULL),
+	(4, '0', 0, '567', 'perd', 'mk', NULL),
+	(5, '0', 0, '232133', 'sejarah', 'bws124', NULL);
 /*!40000 ALTER TABLE `orderbook` ENABLE KEYS */;
 
 -- Dumping structure for table newsystem.user
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(50) DEFAULT NULL,
@@ -76,14 +76,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `imagefile` varchar(50) DEFAULT NULL,
   `contact` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Dumping data for table newsystem.user: ~2 rows (approximately)
+-- Dumping data for table newsystem.user: ~4 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id`, `email`, `username`, `password`, `accesslevel`, `imagefile`, `contact`) VALUES
-	(0, 'abu@gmail.com', 'abu', '123', 'user', NULL, NULL),
+	(0, 'abu@gmail.com', 'abu', '202cb962ac59075b964b07152d234b70', 'public', NULL, NULL),
 	(1, 'ali@gmail.com', 'ali', '202cb962ac59075b964b07152d234b70', 'admin', NULL, NULL),
-	(2, 'exp@gmail.com', 'ahmad', NULL, 'user', NULL, NULL);
+	(2, 'exp@gmail.com', 'ahmad', '123', 'user', NULL, '1234'),
+	(3, 'samsul@gmail.com', 'samsul', '81dc9bdb52d04dc20036dbd8313ed055', 'seller', NULL, NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
